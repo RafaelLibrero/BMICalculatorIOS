7f811d9742865737dbe76e4ca4615e43829d37ed
RafaelLibrero <librerorafael@outlook.com>
2025-06-10 18:08:58 +0200

    BMI App completed

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var weightlabel: UILabel!
-    @IBOutlet weak var heightlabel: UILabel!
-    @IBOutlet weak var resultlabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     var weight: Float = 70.0
     var height: Float = 170.0
@@ -23,18 +24,61 @@ class ViewController: UIViewController {
 
     @IBAction func onWeightChanged(_ sender: UIStepper) {
         weight = Float(sender.value)
-        weightlabel.text = "\(Int(weight)) kg"
+        weightLabel.text = "\(Int(weight)) kg"
     }
     
     @IBAction func onHeightChanged(_ sender: UISlider) {
         height = sender.value
-        heightlabel.text = "\(Int(height)) cm"
+        heightLabel.text = "\(Int(height)) cm"
     }
     
     @IBAction func calculateButton(_ sender: Any) {
         let heighInMeter: Float = height / Float(100)
         let bmi: Float = weight / pow(heighInMeter, 2)
-        resultlabel.text = String(format: "%.2f", bmi)
+        resultLabel.text = String(format: "%.2f", bmi)
+        
+        var color = UIColor.black
+        var showAlert = false
+        
+        switch bmi {
+        case ..<18.5:
+            descriptionLabel.text = "Underweight"
+            color = UIColor.bmiUnderweight
+        case 18.5..<25:
+            descriptionLabel.text = "Normal weight"
+            color = UIColor.bmiNormalWeight
+        case 25..<30:
+            descriptionLabel.text = "Overweight"
+            color = UIColor.bmiOverweight
+        case 30..<35:
+            descriptionLabel.text = "Obese"
+            color = UIColor.bmiObesity
+            showAlert = true
+        default:
+            descriptionLabel.text = "Extreme obesity"
+            color = UIColor.bmiExtremeObesity
+            showAlert = true
+        }
+        
+        resultLabel.textColor = color
+        descriptionLabel.textColor = color
+        
+        if showAlert {
+            let alert = UIAlertController(
+                title: "Riesgo de salud",
+                message: "Tu peso actual pone en grave riesgo tu vida. Busca ayuda médica cuanto antes.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "Mas información", style: .default, handler: { action in
+                // He pulsado mas información
+                let adviceSiteUrl = "https://medlineplus.gov/spanish/ency/patientinstructions/000348.htm"
+                if let url = URL(string: adviceSiteUrl), UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url)
+                }
+            }))
+            alert.addAction(UIAlertAction(title: "Cerrar", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
 
